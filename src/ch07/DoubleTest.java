ec3c0dda3ceee7ffe530e5c8370dd5864d4d6317
ohsangsoo <tkdtn3264@gmail.com>
2023-03-16 14:53:22 +0900

    부동소수점 실습하

---
+++ src/ch07/DoubleTest.java
@@ -0,0 +1,14 @@
+package ch07;
+
+public class DoubleTest {
+
+	public static void main(String[] args) {
+		double dnum = 3.14;
+		float fnum = 3.14f; //float 4바이트 이고 뒤에 f를 식별자 를 써야한다. 
+		// 실수의 리터럴은 일반적으로 double로 인식 하고 float를 쓸 경우 실수 뒤에 f 식별자 를 쓴다.
+	System.out.println(dnum);// double 출력 
+	System.out.println(fnum);// float출력
+		
+	}
+
+}
