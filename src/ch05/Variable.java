3ff153964b354aacd0d7c06b9cbdcf7327e674e1
ohsangsoo <tkdtn3264@gmail.com>
2023-03-16 13:26:08 +0900

    integer 실습하기

---
+++ src/ch05/Variable.java
@@ -0,0 +1,19 @@
+package ch05;
+
+public class Variable {
+
+	public static void main(String[] args) {
+		// 변수 
+		// 프로그램에서 항상 변하는 값을 나타낼때 변수라고한다.
+		// 표현하는 자료가 숫자 문자 문자열 등 다양한 수 있으므로 그에 맞게 자료형을 선언한다.
+		// 표현 하려는 수에 맞는 데이터(자료형)을 변수를 선언
+		int age, count; // integer 선언하기 count
+		age =10;
+		int level=10000;
+		
+		System.out.println(age); //age
+		System.out.println(level); // level 출력
+		
+	}
+
+}
