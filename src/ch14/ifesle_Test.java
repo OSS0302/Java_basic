ad8e4a05cf205901b332797e29152da8a23976d0
ohsangsoo <tkdtn3264@gmail.com>
2023-03-18 15:54:27 +0900

    제어문 if 문 연습하기

---
+++ src/ch14/ifesle_Test.java
@@ -0,0 +1,17 @@
+package ch14;
+
+public class ifesle_Test {
+
+	public static void main(String[] args) {
+		// 초등학생이 입학하는 나이 
+		int age =6;
+		if (age >=8) {
+			System.out.println("학교에 다닙니다.");
+		
+		}else {
+			System.out.println("학교에 다니지 않습니다. ");
+		}
+		System.out.println("노는게 제일 좋아  ");
+	}
+
+}
