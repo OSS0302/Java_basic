d7a22548a3a0bc1ef650233219612b3e8e3772a4
ohsangsoo <tkdtn3264@gmail.com>
2023-03-20 20:38:58 +0900

    주문 클래스 생성하기

---
+++ Chapter2/src/ch1/Order.java
@@ -0,0 +1,13 @@
+package ch1;
+
+public class Order {
+	int orderId;// 주문 아이디
+	String buyerId; //구매자 아이디
+	String sellerId; //판매자 아이디
+	int productId; //상품 코드
+	String orderDate; // 주문일 
+	
+	
+	
+
+}
