d700cdf15849204f12cf3a609bbac9991a8d3ebe
ohsangsoo <tkdtn3264@gmail.com>
2023-03-20 20:40:08 +0900

    학생 클래스 생성하기

---
+++ Chapter2/src/ch1/Student.java
@@ -0,0 +1,15 @@
+package ch1;
+
+public class Student {
+	//package 는 소문자 
+	// 클래스는 대문자 시작한다.
+	// 변수는 메서드 는 소문자로 시작해서 중간중간 대문자 카멜 컨벤션 암묵적인 규
+	int studentNumber; //학생 넘버
+	String studentName;//학생 이름
+	int majorCode;// 전공코드
+	int grade; // 학년
+	
+	
+	
+
+}
