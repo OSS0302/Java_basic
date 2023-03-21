17509c5a2e08de11f3c10c8084588add80ce512b
ohsangsoo <tkdtn3264@gmail.com>
2023-03-21 18:43:10 +0900

    주문 테스트하기

---
+++ Chapter2/src/ch6/OrderTest.java
@@ -0,0 +1,10 @@
+package ch6;
+
+public class OrderTest {
+
+	public static void main(String[] args) {
+		OrderInfo  order = new OrderInfo("2020110200003", "01023450001", "서울시 강남구 역삼동 111-333",20201102, 130258, 35000, "0003");
+		System.out.println(order.showOrderInfo());
+	}
+
+}
