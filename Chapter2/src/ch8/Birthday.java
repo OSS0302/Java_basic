283ba2cefcc492967632aa634438902377c81f90
ohsangsoo <tkdtn3264@gmail.com>
2023-03-22 16:04:21 +0900

    날짜가 유효한 날짜인지 확인하는 기능

--- Chapter2/src/ch8/Birthday.java
+++ Chapter2/src/ch8/Birthday.java
@@ -5,7 +5,9 @@ public class Birthday {
 	private int month;
 	private int year;
 	// getter and setter 는 데이터 가져오고 집어넣을 때쓴다. 수정할떄도 쓴다 .
-	 public int getDay() {
+	private boolean isValid; 
+	
+	public int getDay() {
 		 return day;
 	 }
 	public void setDay(int day) {
@@ -15,7 +17,15 @@ public class Birthday {
 		return month;
 	}
 	public void setMonth(int month) {
-		this.month = month;
+		if (month <1 || month >12) {
+			
+			isValid= false;
+		
+		}else {
+			isValid= true;
+			this.month =month;
+		}
+		
 	}
 	public int getYear() {
 		return year;
@@ -23,6 +33,13 @@ public class Birthday {
 	public void setYear(int year) {
 		this.year = year;
 	}
-	
+	public void showDate() 
+	{
+		if(isValid) {
+			System.out.println(year+"년 "+month+"월 "+day+"입니다.");
+		}else {
+			System.out.println("유효하지 않은 날짜입니다.");
+		}
+	}
 	
 }
