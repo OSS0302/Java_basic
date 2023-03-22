ac721e48961d038bb8b7ea2456fde0e13bef90b3
ohsangsoo <tkdtn3264@gmail.com>
2023-03-22 15:48:08 +0900

    getter setter 사용법 알기

---
+++ Chapter2/src/ch8/Birthday.java
@@ -0,0 +1,28 @@
+package ch8;
+
+public class Birthday {
+	private int day;
+	private int month;
+	private int year;
+	// getter and setter 는 데이터 가져오고 집어넣을 때쓴다. 수정할떄도 쓴다 .
+	 public int getDay() {
+		 return day;
+	 }
+	public void setDay(int day) {
+		this.day =day;
+	}
+	public int getMonth() {
+		return month;
+	}
+	public void setMonth(int month) {
+		this.month = month;
+	}
+	public int getYear() {
+		return year;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
+	
+	
+}
