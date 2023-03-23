5739a062d47cd4751059895407f6ab961d2d92f2
ohsangsoo <tkdtn3264@gmail.com>
2023-03-23 13:55:12 +0900

    택시 넘버 기능 추가한 기능 테스트하기

--- Chapter2/src/ch12/HumanTest.java
+++ Chapter2/src/ch12/HumanTest.java
@@ -5,7 +5,7 @@ public class HumanTest {
 	public static void main(String[] args) {
 		Human humanE = new Human("Edward", 20000);
 		
-		Taxi taxi = new Taxi("잘간다 운수 택시 ");
+		Taxi taxi = new Taxi("잘간다 운수 택시 ","21가3564");
 		humanE.takeTaxi(taxi);
 		humanE.showInfo();
 		taxi.showInfo();
