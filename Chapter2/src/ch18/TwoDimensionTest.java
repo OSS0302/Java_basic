07683b95b515df663322d440ddbf7666fbdad269
ohsangsoo <tkdtn3264@gmail.com>
2023-03-25 15:21:41 +0900

    2차 객체 배열 갯수 선언하기

--- Chapter2/src/ch18/TwoDimensionTest.java
+++ Chapter2/src/ch18/TwoDimensionTest.java
@@ -3,7 +3,8 @@ package ch18;
 public class TwoDimensionTest {
 
 	public static void main(String[] args) {
-		int[][] arr = { {1,2,3}, {1,2,3,4}};
+		//int[][] arr = { {1,2,3}, {1,2,3,4}};
+		int[][] arr = new int[3][4]; // 배열 갯수를 지정 해야한다. 3행 4열 
 		int i, j;
 		
 		for(i =0; i<arr.length; i++) { // arr 의 행의 길이를 나타낸다.
