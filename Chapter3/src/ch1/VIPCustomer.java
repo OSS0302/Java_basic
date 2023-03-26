8568247bb1aa6e3fa30f28ee861a743eba28b353
ohsangsoo <tkdtn3264@gmail.com>
2023-03-26 12:45:14 +0900

    VIP 고객 상속 받기

---
+++ Chapter3/src/ch1/VIPCustomer.java
@@ -0,0 +1,18 @@
+package ch1;
+
+public class VIPCustomer extends Customer {
+	
+	
+	
+	double saleRatio; // 할인 율 적용
+	String agentID; // 개인 상담 에이전트
+	
+	public VIPCustomer() {
+		
+		bounsRatio =0.05; // 포인트 적립 0.05 이고
+		saleRatio= 0.1; // 할인율 적용
+		customerGrade ="VIP";
+		
+	}
+
+}
