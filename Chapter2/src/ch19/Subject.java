cc6912da33ff4ccced9726df56e763aa6c246284
ohsangsoo <tkdtn3264@gmail.com>
2023-03-26 11:52:14 +0900

    ArrayList를 활용한 간단한 성적 산출 프로그램 과목클래스

---
+++ Chapter2/src/ch19/Subject.java
@@ -0,0 +1,24 @@
+package ch19;
+
+public class Subject {
+	String name; //과목이름
+	int scorePoint;//과목포인트
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getScorePoint() {
+		return scorePoint;
+	}
+	public void setScorePoint(int scorePoint) {
+		this.scorePoint = scorePoint;
+	}
+	
+	
+	
+
+}
