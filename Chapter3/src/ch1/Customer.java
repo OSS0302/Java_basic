243a487499e4f999c93464a773e76d01ff0de711
ohsangsoo <tkdtn3264@gmail.com>
2023-03-26 12:40:56 +0900

    고객 상속 받기

---
+++ Chapter3/src/ch1/Customer.java
@@ -0,0 +1,23 @@
+package ch1;
+
+public class Customer {
+
+	private int  custmerID;// 고객아이디
+	private String customerName; //고객이름
+	private String customerGrade; // 고객 등급 
+	int bounsPoint ; // 보너스 포인트 
+	double bounsRatio; // 포인트적립 
+	
+	public Customer() 
+	{
+		customerGrade ="SILVER"; // 실버등급
+		bounsRatio = 0.01; //0.01포인트 적립
+	}
+	public int calPrice(int price) {
+		bounsPoint += price *bounsRatio;
+		return price;
+	}
+	public String showCustomer() {
+		return customerName+"님의 등급은"+customerGrade+"이며, 보너스포인트는"+bounsPoint+"입니다.";
+	}
+}	
