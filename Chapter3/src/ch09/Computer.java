be6b712cfbdd33862652a0e32808d3e31a53372e
ohsangsoo <tkdtn3264@gmail.com>
2023-03-28 18:58:03 +0900

    추상클래스 설명 추가하기

--- Chapter3/src/ch09/Computer.java
+++ Chapter3/src/ch09/Computer.java
@@ -3,7 +3,8 @@ package ch09;
 public abstract class Computer {
 	 abstract void display();// 디스플레
 	 abstract void typing(); // 타이핑
-	
+	// 추상메서드로 놓고 그다음에 하위클래스에게 책임을 넘긴다.
+	 
 	void turnOn() { // 전원을 켜는 메서드 
 		System.out.println("전원을 컵니다.");
 		
@@ -11,4 +12,5 @@ public abstract class Computer {
 	void turOff() {// 전원을 끄는 메서드 
 		System.out.println("전원을 끕니다.");
 	}
+	//추상클래스 안에 공통으로 사용할 메서드들을 선언한다.
 }
