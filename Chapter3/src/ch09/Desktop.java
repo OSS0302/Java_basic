6fbdbef3a2acce44b592fc11c36a945c530038ca
ohsangsoo <tkdtn3264@gmail.com>
2023-03-28 18:57:20 +0900

    컴퓨터하위클래스 선언하기

---
+++ Chapter3/src/ch09/Desktop.java
@@ -0,0 +1,17 @@
+package ch09;
+
+public  class Desktop extends Computer {// 추상메서드를포함한 추상 클래스다
+
+	@Override
+	void display() {
+		System.out.println("Desktop display");
+	}
+
+	@Override
+	void typing() {
+		System.out.println("Desktop typing");
+	}
+	
+	  
+
+}
