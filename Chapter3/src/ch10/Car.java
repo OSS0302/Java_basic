70edde90bda65d871ffe4f60d4017a52c3e7944a
ohsangsoo <tkdtn3264@gmail.com>
2023-03-29 18:20:11 +0900

    와이퍼 기능 추가하기

--- Chapter3/src/ch10/Car.java
+++ Chapter3/src/ch10/Car.java
@@ -4,6 +4,7 @@ public abstract class Car {
 	
 	public abstract void drive(); //주행 
 	public abstract void stop(); //브레이크
+	public abstract void wiper(); // 와이퍼추가하
 	
 	public void startCar(){
 		System.out.println("시동을 켭니다.");
@@ -11,9 +12,13 @@ public abstract class Car {
 	public void turnOff() {
 		System.out.println("시동을 끕니다.");
 	}
+	
+	public void washCar() {} //구현된 메서드 이지만 구현 내용이 없는것이다. 
+	
 	final public void run() {
 		startCar();
 		drive();
+		wiper();
 		stop();
 		turnOff();
 		
