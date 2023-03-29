0b020499fc64e5e0192896e451ac688bd60f2af9
ohsangsoo <tkdtn3264@gmail.com>
2023-03-29 18:22:11 +0900

    카테스트하기

--- Chapter3/src/ch10/CarTest.java
+++ Chapter3/src/ch10/CarTest.java
@@ -5,6 +5,7 @@ public class CarTest {
 	public static void main(String[] args) {
 		Car car = new AICar();
 		car.run();
+		System.out.println("======================");
 		Car mCar = new ManualCar();
 		mCar.run();
 		}
