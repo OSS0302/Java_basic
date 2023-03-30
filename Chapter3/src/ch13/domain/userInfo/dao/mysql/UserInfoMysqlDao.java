ad494de548bf14b3908fddfd4ae5fa70a71ad1de
ohsangsoo <tkdtn3264@gmail.com>
2023-03-30 15:46:08 +0900

    UserInfoMySqlDao.java (UserInfoDao 인터페이스를 구현한 MySql 버전 dao)

--- Chapter3/src/ch13/domain/userInfo/dao/mysql/UserInfoMysqlDao.java
+++ Chapter3/src/ch13/domain/userInfo/dao/mysql/UserInfoMysqlDao.java
@@ -1,5 +1,23 @@
 package ch13.domain.userInfo.dao.mysql;
 
-public class UserInfoMysqlDao {
+import ch13.domain.userInfo.UserInfo;
+import ch13.domain.userInfo.dao.UserInfoDao;
+
+public class UserInfoMysqlDao implements UserInfoDao{
+
+	@Override
+	public void insertUserInfo(UserInfo userInfo) { // 글 작성 
+		System.out.println("Insert into Mysql DB userId = "+userInfo.getUserId());
+	}
+
+	@Override
+	public void updateUserInfo(UserInfo userInfo) { // 글 수정 
+		System.out.println("Update into Mysql DB userId = "+userInfo.getUserId());
+	}
+
+	@Override
+	public void deleteUserInfo(UserInfo userInfo) { // 글 삭제 
+		System.out.println("Delete into Mysql DB userId = "+userInfo.getUserId());
+	}
 
 }
