379f78b20af5eca148c19842cc9f952032ff854e
ohsangsoo <tkdtn3264@gmail.com>
2023-03-30 16:09:27 +0900

    UserInfoClient.java (UserInfoDao 인터페이스를 활용하는 클라이언트 프로그램)

---
+++ Chapter3/src/ch13/web/userInfo/UserInfoClient.java
@@ -0,0 +1,36 @@
+package ch13.web.userInfo;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+import ch13.domain.userInfo.UserInfo;
+import ch13.domain.userInfo.dao.UserInfoDao;
+import ch13.domain.userInfo.dao.mysql.UserInfoMysqlDao;
+import ch13.domain.userInfo.dao.oracle.UserInfoOracleDao;
+
+public class UserInfoClient {
+
+	public static void main(String[] args) throws IOException {
+		FileInputStream  fis = new FileInputStream("db.properties");
+		Properties prop =  new Properties();
+		prop.load(fis);
+		// db.properties 값이오라클이냐mysql 이냐 여러가지 따라서 만들겟다.
+		String dbType = prop.getProperty("DBTYPE");
+		UserInfo userInfo = new UserInfo();
+		UserInfoDao userInfoDao = null;
+		
+		if(dbType.equals("ORACLE")) {
+		userInfoDao = new UserInfoOracleDao();
+		
+		}else if(dbType.equals("MYSQL")) {
+			userInfoDao =new UserInfoMysqlDao();
+		}else {
+			
+		}System.out.println("db.orror");
+		return;
+	}
+}
+
+
