52f19490e1613b0e311e56eedcfe57a83727a1d3
ohsangsoo <tkdtn3264@gmail.com>
2023-03-31 14:00:50 +0900

    추상메서드 유저 기능생성클래스

--- Chapter3/src/ch16/PlayerLevel.java
+++ Chapter3/src/ch16/PlayerLevel.java
@@ -2,10 +2,11 @@ package ch16;
 
 public abstract class PlayerLevel {
 	
-	public abstract void run();
-	public abstract void jump();
-	public abstract void turn();
-	public abstract void showLevelMessage();
+	public abstract void run(); // 달리기
+	public abstract void jump();// 점프
+	public abstract void turn();// 돌기
+	public abstract void showLevelMessage(); // 레벨메시지
+	
 	
 	public void go(int count){
 		run();
