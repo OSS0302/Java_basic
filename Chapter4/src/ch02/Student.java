92f8ddb3b7ae20b8383bfa0a7019729068a762b9
ohsangsoo <tkdtn3264@gmail.com>
2023-04-01 21:58:32 +0900

    Object 클래스의 메서드 활용

---
+++ Chapter4/src/ch02/Student.java
@@ -0,0 +1,33 @@
+package ch02;
+
+public class Student {
+	
+	private int studentId; //학생 아이디 
+	private String studentName; // 학생이름 
+
+	public Student(int studentId, String studentName)
+	{
+		this.studentId = studentId;
+		this.studentName = studentName;
+	}
+	
+	public boolean equals(Object obj) {
+		if( obj instanceof Student) {
+			Student std = (Student)obj;
+			if(this.studentId == std.studentId )
+				return true;
+			else return false;
+		}
+		return false;
+		
+	}
+	
+	@Override
+	public int hashCode() {
+		return studentId;
+	}
+}
+
+
+
+
