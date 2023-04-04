b2c6011a00546e0f2e444a135c704276fad9b9b1
ohsangsoo <tkdtn3264@gmail.com>
2023-04-04 13:54:25 +0900

    해쉬 코드 출력하기

--- Chapter4/src/ch02/EqaualTest1.java
+++ Chapter4/src/ch02/EqaualTest1.java
@@ -8,7 +8,9 @@ public class EqaualTest1 {
 		
 		System.out.println(std1 == std2); //다른 객체여서 false 이 뜬다
 		System.out.println(std1.equals(std2)); //문자열이 같은지 확인했을때 주소값이 같은지 확인
-		
+		// 해쉬 코드 출력하기
+		System.out.println(std1.hashCode());
+		System.out.println(std2.hashCode());
 	
 	}
 
