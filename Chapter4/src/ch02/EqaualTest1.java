cb234c4b57245a984336927ada07a1f1e9577392
ohsangsoo <tkdtn3264@gmail.com>
2023-04-04 13:58:03 +0900

    String 객체 만들어서 해쉬코드값 비교하기

--- Chapter4/src/ch02/EqaualTest1.java
+++ Chapter4/src/ch02/EqaualTest1.java
@@ -11,7 +11,15 @@ public class EqaualTest1 {
 		// 해쉬 코드 출력하기
 		System.out.println(std1.hashCode());
 		System.out.println(std2.hashCode());
-	
+		
+		// String 으로 보기
+		String str1 = new String("abc");
+		String str2 = new String("abc");
+		
+		System.out.println(std1.equals(str2));
+		//여기 서 같은 해쉬 코드 값이 나오는걸 알수있다.
+		System.out.println(str1.hashCode());
+		System.out.println(str2.hashCode());
 	}
 
 }
