f38a53b62053d5b9191e3510642dc83b1f909542
ohsangsoo <tkdtn3264@gmail.com>
2023-04-06 16:26:17 +0900

    파우더 제네릭 클래스 생성하기

--- Chapter5/src/ch06/ThreeDPrint1.java
+++ Chapter5/src/ch06/ThreeDPrint1.java
@@ -1,7 +1,24 @@
-package ch06;
+ package ch06;
 
 public class ThreeDPrint1 {
 	
 	private Powder material;// 
+	
+	public Powder getMaterial() {
+		return material;
+		
+	}
+
+	public ThreeDPrint1(Powder material) {
+		this.material = material;
+	}
+
+	@Override
+	public String toString() {
+		return  "재료는 Powder입니다";
+	}
+	
+	
+	
 
 }
