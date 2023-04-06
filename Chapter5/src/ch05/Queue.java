47edf03db68013fe3aeea07027d45905f07cf020
ohsangsoo <tkdtn3264@gmail.com>
2023-04-06 16:04:47 +0900

    인터페이스 주석 추가하기

--- Chapter5/src/ch05/Queue.java
+++ Chapter5/src/ch05/Queue.java
@@ -2,9 +2,9 @@ package ch05;
 
 public interface Queue {
 	
-public void enQueue(String data);
-public String deQueue();
-public void printQueue();
+public void enQueue(String data);  // 큐 String 타입으로 입력하기
+public String deQueue(); // 큐 출력하기
+public void printQueue(); // 큐 보여주기
 
 
 }
