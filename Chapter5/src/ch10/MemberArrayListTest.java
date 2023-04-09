edf0055d1699fec08b2ed7a98c8bee65b8172659
ohsangsoo <tkdtn3264@gmail.com>
2023-04-09 21:24:24 +0900

    멤버 추가하기

--- Chapter5/src/ch10/MemberArrayListTest.java
+++ Chapter5/src/ch10/MemberArrayListTest.java
@@ -5,9 +5,14 @@ public class MemberArrayListTest {
 	public static void main(String[] args) {
 MemberArrayList memberArrayList = new MemberArrayList();
 		
-		Member memberLee = new Member(1001, "이순신" ,"경기도 평택시");
+		Member memberLee = new Member(1001, "이경영" ,"서울특별시 동작");
+		Member memberKim = new Member(1002, "김우빈","서울특별시 강남구");
+		Member memberOh = new Member(1003,"오달수","서울 특별시영등포구 ");
 		
-		memberArrayList.addMember(memberLee);// 이순신 리스트에 넣기
+		
+		memberArrayList.addMember(memberLee);// 이경영 리스트에 넣기
+		memberArrayList.addMember(memberKim);// 김우빈 리스트에 넣기
+		memberArrayList.addMember(memberOh);
 		
 		memberArrayList.showAllMember(); //보
 
