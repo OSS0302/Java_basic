1cc0868ba61b4f2005ca582ff7edee4586637b70
ohsangsoo <tkdtn3264@gmail.com>
2023-04-10 15:26:34 +0900

    iterator 테스트하기

--- Chapter5/src/ch11/MemberArrayListTest.java
+++ Chapter5/src/ch11/MemberArrayListTest.java
@@ -5,9 +5,9 @@ public class MemberArrayListTest {
 	public static void main(String[] args) {
 MemberArrayList memberArrayList = new MemberArrayList();
 		
-		Member memberLee = new Member(1001, "이경영" ,"서울특별시 동작");
-		Member memberKim = new Member(1002, "김우빈","서울특별시 강남구");
-		Member memberOh = new Member(1003,"오달수","서울 특별시영등포구 ");
+		Member memberLee = new Member(1001, "이지연" ,"서울특별시 서초구");
+		Member memberKim = new Member(1002, "김태리","서울특별시 강남구");
+		Member memberOh = new Member(1003,"오나라","서울 특별시영등포구 ");
 		
 		
 		memberArrayList.addMember(memberLee);// 이경영 리스트에 넣기
@@ -16,7 +16,7 @@ MemberArrayList memberArrayList = new MemberArrayList();
 		
 		memberArrayList.showAllMember(); //보기
 		
-		memberArrayList.removeMember(1002);
+		memberArrayList.removeMember(memberOh.getMemberId());
 		
 		memberArrayList.showAllMember(); //보기
 
