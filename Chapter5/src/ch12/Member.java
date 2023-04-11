2f6c128ba92e6861ae04d03b47eb1d9a34c00bea
ohsangsoo <tkdtn3264@gmail.com>
2023-04-11 13:44:35 +0900

    회원 클래스 생성 후 equal과 hashcode 사용

---
+++ Chapter5/src/ch12/Member.java
@@ -0,0 +1,60 @@
+package ch12;
+
+public class Member {
+	private int memberId; // 회원 아이디 
+	private String memberName; //회원 이름
+	private String address; // 회원 주소
+
+	public Member(int memberId, String memberName, String address) {
+		this.memberId = memberId;
+		this.memberName = memberName;
+		this.address = address;
+	}
+
+	public int getMemberId() {
+		return memberId;
+	}
+
+	public void setMemberId(int memberId) {
+		this.memberId = memberId;
+	}
+
+	public String getMemberName() {
+		return memberName;
+	}
+
+	public void setMemberName(String memberName) {
+		this.memberName = memberName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	@Override
+	public String toString() {  //toString 메소드 오버로딩
+		return memberName +"회원아이디는"+memberId +"이고 주소는"+address+"입니다.";
+		
+	}
+
+
+	
+
+	@Override
+	public boolean equals(Object obj) {
+		if(obj instanceof Member) {
+			Member member = (Member)obj; // 멤버로 형변환을 해준다.
+			return true;
+	}
+		else return false;
+		}
+	@Override
+	public int hashCode() {
+	
+	return memberId; // 아이다를 반환 
+		}
+}
