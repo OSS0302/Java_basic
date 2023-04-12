136c65b71749dad73fe2f3905f62303d0f9a6e70
ohsangsoo <tkdtn3264@gmail.com>
2023-04-12 16:28:59 +0900

    문자를 이어주는 메서드 클래스 생성하기

---
+++ Chapter6/src/ch04/StringConcatInpl.java
@@ -0,0 +1,12 @@
+package ch04;
+
+public class StringConcatInpl implements StringConcat{
+
+	@Override
+	public void makeString(String s1, String s2) {
+		System.out.println(s1+","+s2); //출력문 
+	}
+	
+	
+
+}
