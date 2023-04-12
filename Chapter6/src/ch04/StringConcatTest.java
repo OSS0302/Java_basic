95149dfabbc957dc69581dfa203df4fd56941477
ohsangsoo <tkdtn3264@gmail.com>
2023-04-12 16:30:10 +0900

    문자 이어주는 클래스 객체지향프로그래

---
+++ Chapter6/src/ch04/StringConcatTest.java
@@ -0,0 +1,11 @@
+package ch04;
+
+public class StringConcatTest {
+
+	public static void main(String[] args) {
+		StringConcatInpl strImpl = new StringConcatInpl();
+		strImpl.makeString("Hello","chicken "); // 호출하기 
+		
+	}
+
+}
