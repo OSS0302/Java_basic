528216efce632ba0950aeb1bb509b7bcf7d46e4c
ohsangsoo <tkdtn3264@gmail.com>
2023-04-12 15:55:32 +0900

    @FunctionalInterface // 어노테이션은 컴파일러에게 정보를 주는 역할과 오류를 방지하는 역할도 한다.
    //메서드를 두개이상 쓰면 오류가 난다. @FunctionalInterface 떄문에

--- Chapter6/src/ch02/Add.java
+++ Chapter6/src/ch02/Add.java
@@ -1,7 +1,7 @@
 package ch02;
-
-public interface Add {
+@FunctionalInterface // 어노테이션은 컴파일러에게 정보를 주는 역할과 오류를 방지하는 역할도 한다.
+public interface Add { //메서드를 두개이상 쓰면 오류가 난다. @FunctionalInterface 떄문에
 	
 	public int add(int x, int y);
-	
+	//public int sub(int x , int y);
 }
