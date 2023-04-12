a3d3fccf38e85704de04b248266f376306b3e475
ohsangsoo <tkdtn3264@gmail.com>
2023-04-12 16:44:38 +0900

    람다식 말을 이어주는인터페이스 생성하기

--- Chapter6/src/ch04/StringConcat.java
+++ Chapter6/src/ch04/StringConcat.java
@@ -1,7 +1,8 @@
 package ch04;
-
+@FunctionalInterface
 public interface StringConcat {
-	public void makeString(String s1, String s2 ); // s1 과 s2 의 연결해주는 문자를  반환 하는 인터페이스 
+	public void makeString(String s1, String s2 ); 
+	// s1 과 s2 의 연결해주는 문자를  반환 하는 인터페이스 
 	
 	
 }
