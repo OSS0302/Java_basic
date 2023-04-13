1829c8df054e1a8ee4541bf1308edb654f8e519e
ohsangsoo <tkdtn3264@gmail.com>
2023-04-13 15:08:03 +0900

    스트림에 map를 이용해서 이름 만 추출하기

---
+++ Chapter6/src/ch07/CustomerTest.java
@@ -0,0 +1,24 @@
+package ch07;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CustomerTest {
+
+	public static void main(String[] args) {
+		Customer customerLee = new Customer("이모씨", 40, 1000000);
+		Customer customerkim = new Customer("김모씨", 45, 1000000);
+		Customer customerHong = new Customer("홍모씨", 44, 1000000);
+		
+		List<Customer> customerList = new ArrayList<>();
+		customerList.add(customerHong);
+		customerList.add(customerkim);
+		customerList.add(customerLee);
+		
+		// 고객 출력하기 
+		System.out.println("고객 명단 출력하기");
+
+		customerList.stream().map(m->m.getName()).forEach(s->System.out.println(s));
+	}
+
+}
