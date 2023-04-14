a2327882b08f2a94d93679220d0ed546aa8f1954
ohsangsoo <tkdtn3264@gmail.com>
2023-04-14 19:32:36 +0900

    프로그래머가 예외를 처리해줘야 하는 예 (배열의 오류 처리)

---
+++ Chapter6/src/ch08/ArrayExceptionHandling.java
@@ -0,0 +1,18 @@
+package ch08;
+
+public abstract class ArrayExceptionHandling {
+
+	public static void main(String[] args) {
+		int[] arr = {1,2,3,4,5};
+		try{
+			for(int i=0; i<=5; i++){
+				System.out.println(arr[i]);
+			}
+		}catch(ArrayIndexOutOfBoundsException e){
+			System.out.println(e);
+		}
+		System.out.println("비정상 종료되지 않았습니다.");
+	}
+
+
+}
