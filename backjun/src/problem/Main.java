30ef3d54d5467161b0114bb2c73e7c71ffb4f38a
ohsangsoo <tkdtn3264@gmail.com>
2023-05-07 16:37:12 +0900

    백준 문제풀기 frist commit

---
+++ backjun/src/problem/Main.java
@@ -0,0 +1,27 @@
+package problem;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+	
+		Scanner scanner = new Scanner(System.in);
+
+	      int hour = scanner.nextInt(); // 시
+	      int minute = scanner.nextInt(); // 분
+	      scanner.close();
+
+	      if(minute < 45) {
+	          hour--; // 
+	          minute = 60 - (45 - minute); 
+
+	          if(hour < 0) {
+	            hour = 23;
+	          }
+	          System.out.println(hour + " " + minute);
+
+	      }else {
+	          System.out.println(hour + " " + (minute - 45));
+	      }
+	  }
+	}
