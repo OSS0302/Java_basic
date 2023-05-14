87bdee73ccb6b729503b1523fb844eb89d5b5210
ohsangsoo <tkdtn3264@gmail.com>
2023-05-14 21:52:44 +0900

    백준 10925

---
+++ backjun/src/problem10925/Main.java
@@ -0,0 +1,14 @@
+package problem10925;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scanner =new Scanner(System.in);
+		String word = scanner.next();
+		System.out.printf(word+"??!");
+		scanner.close(); //스캔함수 를 닫아야한다.
+	}
+
+}
+
